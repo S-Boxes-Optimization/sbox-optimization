41c4a6299edf05649aa9f06105897efe97ca064b
Juan Guevara <soyjuandavidguevara@gmail.com>
2025-05-07 06:30:29 -0500

    Displaying initial best

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from search.local_search import Search as LocalSearch
 box = DESBox()
 
 search = LocalSearch(box)
+table, std = ddt(search.representation, box.input_size, box.output_size)
+print("Std:", std, "Initial Sbox:")
 DESBox.print_from_representation(search.representation)
-display_ddt(ddt(search.representation, box.input_size, box.output_size)[0])
+display_ddt(table)
 search.search(100)
 final_ddt, final_std = ddt(search.best, box.input_size, box.output_size)
+print("Std:", final_std, "Final Sbox:")
 display_ddt(final_ddt)
 
 print("Sbox:")
